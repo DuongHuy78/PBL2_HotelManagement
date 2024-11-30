94c13f529f70ee2c6e11151847558098bce2e915
Guen Nhat King <guennhatking@gmail.com>
2024-11-30 21:53:47 +0700

    Save code

--- source/header/Node.h
+++ source/header/Node.h
@@ -12,14 +12,14 @@ public:
 };
 
 template <class T> Node<T>::Node() {
-    this->next = NULL;
-    this->prev = NULL;
+    this->next = nullptr;
+    this->prev = nullptr;
 }
 
 template <class T> Node<T>::Node(T data) {
     this->data = data;
-    this->next = NULL;
-    this->prev = NULL;
+    this->next = nullptr;
+    this->prev = nullptr;
 }
 
 template <class T> Node<T>::~Node() {
